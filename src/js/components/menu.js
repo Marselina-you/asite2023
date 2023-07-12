0df84742ff375d05475b080fed952adaa4728e75
Marselina-you <marselina88@list.ru>
2023-07-12 19:53:20 +0300

    feedback and certification

--- src/js/components/menu.js
+++ src/js/components/menu.js
@@ -4,3 +4,49 @@ const menuMobile = document.querySelector('.header__menu-list');
 menuBtn.addEventListener('click', ()=> {
   menuMobile.classList.toggle('menu--open');
 });
+const swiperOne = new Swiper('.feedback__slider', {
+  // Optional parameters
+
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+
+  },
+
+
+
+});
+
+const swiperTwo = new Swiper('.certification__slider', {
+  // Optional parameters
+
+  loop: true,
+  slidesPerView: 3,
+  spaceBetween: 20,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 3
+
+    },
+    480: {
+      slidesPerView: 2
+
+    },
+    360: {
+      slidesPerView: 1
+
+    },
+  },
+
+
+});
